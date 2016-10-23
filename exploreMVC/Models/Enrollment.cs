699852734d8b507cc01a6b934582afdbedd11d46
Farid <farid.skyman@gmail.com>
2016-10-23 22:59:59 +0800

    added EF and link up to DB

---
+++ exploreMVC/Models/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace exploreMVC.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; } // foreign key
+        public int StudentID { get; set; } //foreign key to student table
+        public Grade? Grade { get; set; } //? means its nullable
+
+        public virtual Course Course { get; set; } //
+        public virtual Student Student { get; set; }
+    }
+}
