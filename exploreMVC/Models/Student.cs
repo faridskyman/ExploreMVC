2efe41b1c7d29ee96d50d4eb8b8a738f689bcf4f
Farid <farid.skyman@gmail.com>
2016-10-24 00:00:19 +0800

    added details page and stopped at (Page 41)

--- exploreMVC/Models/Student.cs
+++ exploreMVC/Models/Student.cs
@@ -11,6 +11,7 @@ namespace exploreMVC.Models
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public DateTime EnrollmentDate { get; set; }
+        public string Email { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
