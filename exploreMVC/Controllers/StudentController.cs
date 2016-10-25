5b35811c12622e968d554abaed74d7e6d36fd190
Farid <farid.skyman@gmail.com>
2016-10-25 22:45:43 +0800

    finished to paye 50

--- exploreMVC/Controllers/StudentController.cs
+++ exploreMVC/Controllers/StudentController.cs
@@ -47,13 +47,20 @@ namespace exploreMVC.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,LastName,FirstName,EnrollmentDate")] Student student)
+        public ActionResult Create([Bind(Include = "LastName,FirstName,EnrollmentDate")] Student student)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.Students.Add(student);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException /*dex*/)
+            {
+                ModelState.AddModelError("", "Unable to add pls try again");
             }
 
             return View(student);
@@ -81,11 +88,18 @@ namespace exploreMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "ID,LastName,FirstName,EnrollmentDate")] Student student)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    db.Entry(student).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException /*dex*/)
+            {
+                ModelState.AddModelError("", "cant save, try again");
             }
             return View(student);
         }
